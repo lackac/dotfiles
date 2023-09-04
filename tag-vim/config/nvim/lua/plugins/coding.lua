cdb8405aad350bd61f2b06ad51ad6823a8292544
Laszlo Bacsi <laszlo.bacsi@100starlings.com>
2023-09-04 14:39:35 +0200

    Disable supertab completion

--- tag-vim/config/nvim/lua/plugins/coding.lua
+++ tag-vim/config/nvim/lua/plugins/coding.lua
@@ -1,8 +1,3 @@
-local check_backspace = function()
-  local col = vim.fn.col(".") - 1
-  return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
-end
-
 return {
   {
     "L3MON4D3/LuaSnip",
@@ -19,44 +14,45 @@ return {
       "hrsh7th/cmp-nvim-lua",
     },
     opts = function(_, opts)
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
       local cmp = require("cmp")
       local luasnip = require("luasnip")
+
       opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "nvim_lua" } }))
-      opts.mapping["<C-p>"] = cmp.mapping.select_prev_item()
-      opts.mapping["<C-n>"] = cmp.mapping.select_next_item()
-      opts.mapping["<C-Tab>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" })
-      opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_next_item()
-        elseif luasnip.expandable() then
-          luasnip.expand()
-        elseif luasnip.expand_or_jumpable() then
-          luasnip.expand_or_jump()
-        elseif check_backspace() then
-          fallback()
-        else
-          fallback()
-        end
-      end, {
-        "i",
-        "s",
+
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          -- if cmp.visible() then
+          --   cmp.select_next_item()
+          if luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.mapping.confirm({ select = false })
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          -- if cmp.visible() then
+          --   cmp.select_prev_item()
+          if luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
       })
-      cmp.mapping["<S-Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        elseif luasnip.jumpable(-1) then
-          luasnip.jump(-1)
-        else
-          fallback()
-        end
-      end, {
-        "i",
-        "s",
-      })
+
       opts.confirm_opts = {
         behavior = cmp.ConfirmBehavior.Replace,
         select = false,
       }
+
       opts.window = {
         completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
