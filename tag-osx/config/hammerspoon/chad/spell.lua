78bfca1390afc20ea9aa6e8dec6128681f5724aa
Laszlo Bacsi <laszlo.bacsi@100starlings.com>
2023-10-27 12:40:18 +0200

    Chooser plugin for spell checking

---
+++ tag-osx/config/hammerspoon/chad/spell.lua
@@ -0,0 +1,46 @@
+local dictionary = require("hs.dictionary")
+local nerdFontsIcon = require("ext.images").nerdFontsIcon
+
+local module = {
+  keyword = "spell",
+  useFzf = true,
+  tip = { text = "type spell⇥ to check correct spelling of word" },
+}
+
+local log
+
+local spellCheckIcon = nerdFontsIcon("󰓆", "brown")
+
+module.compileChoices = function(query)
+  log.v("compileChoices " .. hs.inspect(query))
+
+  if #query > 1 then
+    local guesses = dictionary.guesses(query)
+    return hs.fnutils.map(guesses, function(guess)
+      return {
+        text = guess,
+        id = "guess:" .. guess,
+        source = module.requireName,
+        image = spellCheckIcon,
+      }
+    end)
+  else
+    return {}
+  end
+end
+
+module.complete = function(choice)
+  log.v("complete choice: " .. hs.inspect(choice))
+  if choice then
+    hs.pasteboard.setContents(choice.text)
+  end
+end
+
+module.start = function(main, _)
+  module.main = main
+  log = hs.logger.new(module.requireName, "verbose")
+end
+
+module.stop = function() end
+
+return module
