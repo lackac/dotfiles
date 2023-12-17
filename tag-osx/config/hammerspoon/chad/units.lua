010e910da26febe1cab4d6c8e8cf848a0a0d85d3
Laszlo Bacsi <laszlo.bacsi@100starlings.com>
2023-12-17 22:18:18 +0100

    Better currency conversion with defaults

--- tag-osx/config/hammerspoon/chad/units.lua
+++ tag-osx/config/hammerspoon/chad/units.lua
@@ -1,5 +1,5 @@
 local capture = require("ext.utils").capture
-local nerdFontsIcon = require("ext.images").nerdFontsIcon
+local images = require("ext.images")
 
 local cache = {}
 local module = {
@@ -12,7 +12,54 @@ local module = {
 local log
 
 local unitsPath = "/opt/homebrew/bin/gunits"
-local unitsImage = nerdFontsIcon("󰯍", "chocolate")
+local unitsImage = images.nerdFontsIcon("󰯍", "chocolate")
+
+local categories = {
+  {
+    name = "currency",
+    units = {
+      "HUF",
+      "GBP",
+      "EUR",
+      "USD",
+      "CHF",
+      "DKK",
+      "CZK",
+      "NOK",
+      "SEK",
+      "PLN",
+      "RON",
+      "UAH",
+      "RUB",
+      "AED",
+      "AUD",
+      "CNY",
+      "JPY",
+      "KRW",
+      "INR",
+      "CAD",
+      "HKD",
+    },
+    defaults = { "HUF", "GBP", "EUR", "USD" },
+    transformQuery = string.upper,
+    unitImage = images.flagIcon,
+  },
+}
+
+local function findCategory(unit)
+  if unit == nil or unit == "" then
+    return
+  end
+
+  for _, category in ipairs(categories) do
+    if category.transformQuery then
+      unit = category.transformQuery(unit)
+    end
+    if hs.fnutils.contains(category.units, unit) then
+      return category
+    end
+  end
+end
 
 local function evaluateExpression(expr)
   log.vf("evaluating '%s' with gunits", expr)
@@ -26,25 +73,54 @@ local function evaluateExpression(expr)
     command = string.format("%s --terse '%s'", unitsPath, expr:gsub("'", "'\\''"))
   end
 
-  return capture(command)
+  return capture(command):gsub("%s+$", "")
 end
 
 module.compileChoices = function(query)
   log.v("compileChoices " .. hs.inspect(query))
 
+  query = query:gsub("^ +", ""):gsub(" +$", "")
+
   if query:len() > 1 then
-    local result = evaluateExpression(query)
-    log.vf("result => %s", result)
-    if result then
-      return {
-        {
+    local fromUnit = query:match("[%d. ]+([^%d ]+)")
+    local toUnit = query:match("[%d. ]+[^%d ]+ +([^%d ]+)$")
+
+    local category = findCategory(fromUnit)
+    if category and category.transformQuery then
+      fromUnit = category.transformQuery(fromUnit)
+      query = category.transformQuery(query)
+    end
+
+    local results = {}
+    if category and not toUnit then
+      for i, unit in ipairs(category.defaults) do
+        if unit ~= fromUnit then
+          local result = evaluateExpression(query .. " " .. unit)
+          log.vf("result => %s", result)
+          if result then
+            table.insert(results, {
+              text = result .. " " .. unit,
+              id = "units-result-" .. i,
+              source = module.requireName,
+              image = category and category.unitImage and category.unitImage(unit) or unitsImage,
+            })
+          end
+        end
+      end
+    else
+      local result = evaluateExpression(query)
+      log.vf("result => %s", result)
+      if result then
+        table.insert(results, {
           text = result,
           id = "units-result",
           source = module.requireName,
-          image = unitsImage,
-        },
-      }
+          image = category and category.unitImage and category.unitImage(toUnit) or unitsImage,
+        })
+      end
     end
+
+    return results
   else
     return {}
   end
@@ -59,7 +135,7 @@ end
 
 module.start = function(main, _)
   module.main = main
-  log = hs.logger.new(module.requireName, "debug")
+  log = hs.logger.new(module.requireName, "verbose")
 end
 
 module.stop = function() end
